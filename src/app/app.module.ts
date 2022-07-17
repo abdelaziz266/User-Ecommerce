c4f689c9c7b245d90de8bda9cb5a7df5f50ee986
abdelaziz266 <zizoelbeltagy266@gmail.com>
2022-07-17 02:26:15 +0200

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CartComponent } from './component/cart/cart.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ProductDetailsComponent } from './component/product-details/product-details.component';
+import { ProductsComponent } from './component/products/products.component';
+import { SelectComponent } from './component/select/select.component';
+import { SpinnerComponent } from './component/spinner/spinner.component';
+import { ViewProductsComponent } from './component/view-products/view-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HeaderComponent,
+    ProductDetailsComponent,
+    ProductsComponent,
+    SelectComponent,
+    SpinnerComponent,
+    ViewProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
